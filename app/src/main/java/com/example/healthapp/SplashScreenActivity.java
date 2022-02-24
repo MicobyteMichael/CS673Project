57eab9df06700be232db53519d0545fce53425ec
Michael Tuma <mike.tuma@hotmail.com>
2022-02-24 16:38:18 -0500

    Fixed spelling mistake

--- app/src/main/java/com/example/healthapp/SplashScreenActivity.java
+++ app/src/main/java/com/example/healthapp/SplashScreenActivity.java
@@ -2,18 +2,17 @@ package com.example.healthapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.AlarmManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 
-public class SpalshScreenActivity extends AppCompatActivity {
+public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_spalsh_screen);
+        setContentView(R.layout.activity_splash_screen);
 
         Handler.createAsync(Looper.getMainLooper()).postDelayed(this::showNextScreen, 1500);
     }
