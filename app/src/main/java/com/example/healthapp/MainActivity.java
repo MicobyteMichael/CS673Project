3c13b5a203dbd00f44bf7307fe0f51d1320f43f2
Michael Tuma <mike.tuma@hotmail.com>
2022-02-24 16:35:22 -0500

    Disable back button on main screen to fix issue where it goes back to the login screen

--- app/src/main/java/com/example/healthapp/MainActivity.java
+++ app/src/main/java/com/example/healthapp/MainActivity.java
@@ -6,9 +6,15 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    public MainActivity() {
+        super(R.layout.activity_main);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public void onBackPressed() {}
 }
