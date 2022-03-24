376c48d8d14b6660eceaa0f892d02ba4ce4c8791
Michael Tuma <mike.tuma@hotmail.com>
2022-03-23 23:34:24 -0400

    Added Java code for the meal submission fragment

---
+++ app/src/main/java/com/example/healthapp/ui/meals/SubmitMealFragment.java
@@ -0,0 +1,12 @@
+package com.example.healthapp.ui.meals;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.healthapp.R;
+
+public class SubmitMealFragment extends Fragment {
+
+    public SubmitMealFragment() {
+        super(R.layout.fragment_submit_meal);
+    }
+}
