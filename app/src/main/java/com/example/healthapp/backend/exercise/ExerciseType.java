5f356adc8ce54a6c0fcad5f5c3fdeefe96b4be30
Michael Tuma <mike.tuma@hotmail.com>
2022-03-24 21:31:25 -0400

    Added more exercises

--- app/src/main/java/com/example/healthapp/backend/exercise/ExerciseType.java
+++ app/src/main/java/com/example/healthapp/backend/exercise/ExerciseType.java
@@ -2,11 +2,17 @@ package com.example.healthapp.backend.exercise;
 
 public enum ExerciseType {
 
-    Walking ("Just walking, at a normal, steady pace.",                    "distance", "miles", false, DifficultyLevel.Moderate),
-    Jogging ("Jogging at a moderate pace",                                 "distance", "miles", false, DifficultyLevel.Strenuous),
-    Running ("Running at a very fast pace",                                "distance", "miles", false, DifficultyLevel.VeryStrenuous),
-    Swimming("Doing laps in a standard-size pool",                         "laps",      "",     true,  DifficultyLevel.Strenuous),
-    Cycling ("Riding with a mix of inclines, declines, and flat sections", "distance", "miles", false, DifficultyLevel.Moderate);
+    Walking     ("Just walking, at a normal, steady pace.",                    "distance", "miles", false, DifficultyLevel.Moderate),
+    Jogging     ("Jogging at a moderate pace",                                 "distance", "miles", false, DifficultyLevel.Strenuous),
+    Running     ("Running at a very fast pace",                                "distance", "miles", false, DifficultyLevel.VeryStrenuous),
+    Swimming    ("Doing laps in a standard-size pool",                         "laps",     "",      true,  DifficultyLevel.Strenuous),
+    Cycling     ("Riding with a mix of inclines, declines, and flat sections", "distance", "miles", false, DifficultyLevel.Moderate),
+    JumpRope    ("Jumping rope at a moderate pace",                            "jumps",    "",      true,  DifficultyLevel.Moderate),
+    Squats      ("Squatting from a standing position, then standing back up",  "squats",   "",      true,  DifficultyLevel.Moderate),
+    JumpingJacks("Jumping up and simultaneously moving your arms",             "jumps",    "",      true,  DifficultyLevel.Strenuous),
+    PushUps     ("Doing standard pushups and keeping your back straight",      "push-ups", "",      true,  DifficultyLevel.Strenuous),
+    SitUps      ("Laying on your back and moving into a sitting position",     "sit-ups",  "",      true,  DifficultyLevel.Moderate),
+    PullUps     ("Pulling yourself up by your arms and clearing the bar",      "pull-ups", "",      true,  DifficultyLevel.VeryStrenuous);
 
     // Calories per hour based on https://www.onhealth.com/content/1/calories_burned_during_fitness
     // Heart rate based on https://www.mayoclinic.org/healthy-lifestyle/fitness/in-depth/exercise-intensity/art-20046887 and https://www.mayoclinic.org/healthy-lifestyle/fitness/expert-answers/heart-rate/faq-20057979
